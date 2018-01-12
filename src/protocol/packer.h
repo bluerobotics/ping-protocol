1a03ac91e86d0ac8cce6d52dd980c87daa44bb13
Patrick José Pereira <patrickelectric@gmail.com>
2018-01-12 17:50:17 -0200

    Packer: Organize merge
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/protocol/packer.h
+++ src/protocol/packer.h
@@ -12,7 +12,7 @@ public:
     QByteArray pack(const QByteArray& packString, const QVariant& var);
     QByteArray pack(const QByteArray& packString, const QVariantList& varList);
     QByteArray convert(const QVariant& var, const QChar& format);
+    QByteArray merge(const QByteArray& header, const QByteArray& payload);
     QByteArray populateHeader(int messageID = 120, int srcDevID = 0, int dstDevID = 0, int payload = -1);
     QByteArray request(const QVariant& messageID, int srcDevID = 0, int dstDevID = 0);
-    QByteArray merge(const QByteArray& header, const QByteArray& payload);
 };
