5da32ab032cce7ff25afa2e8833f83f8a28734f8
Patrick José Pereira <patrickelectric@gmail.com>
2018-01-14 22:18:16 -0200

    Protocol: Add a series of functions and signals
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/protocol/protocol.cpp
+++ src/protocol/protocol.cpp
@@ -32,8 +32,86 @@ void Protocol::handleData(const QByteArray& data)
     _packer->decode(data);
 }
 
+void Protocol::request(QVariant messageID)
+{
+    auto byteArray = _packer->request(messageID);
+    emit sendData(byteArray);
+}
+
+// General requests
 void Protocol::requestVersion()
 {
-    auto byteArray = _packer->request(Message::GeneralMessageID::gen_get_version);
-    emit sendData(byteArray);
+    request(Message::GeneralMessageID::gen_get_version);
+}
+
+void Protocol::requestDeviceID()
+{
+    request(Message::GeneralMessageID::gen_device_id);
+}
+
+void Protocol::requestNewData()
+{
+    request(Message::GeneralMessageID::gen_new_data);
+}
+
+// Echosounder requests
+void Protocol::requestEchosounderDistanceSimple()
+{
+    request(Message::EchosounderMessageID::es_distance_simple);
+}
+
+void Protocol::requestEchosounderDistance()
+{
+    request(Message::EchosounderMessageID::es_distance);
+}
+
+void Protocol::requestEchosounderProfile()
+{
+    request(Message::EchosounderMessageID::es_profile);
+}
+
+void Protocol::requestEchosounderRange()
+{
+    request(Message::EchosounderMessageID::es_range);
+}
+
+void Protocol::requestEchosounderMode()
+{
+    request(Message::EchosounderMessageID::es_mode);
+}
+
+void Protocol::requestEchosounderRate()
+{
+    request(Message::EchosounderMessageID::es_rate);
+}
+
+void Protocol::requestEchosounderGain()
+{
+    request(Message::EchosounderMessageID::es_gain);
+}
+
+void Protocol::requestEchosounderPulse()
+{
+    request(Message::EchosounderMessageID::es_pulse);
+}
+
+// Mechanical Scanning Sonar requests
+void Protocol::requestMSSAngleProfilea()
+{
+    request(Message::MechanicalScanningSonarMessageID::mss_angle_profile);
+}
+
+void Protocol::requestMSSRange()
+{
+    request(Message::MechanicalScanningSonarMessageID::mss_range);
+}
+
+void Protocol::requestMSSMode()
+{
+    request(Message::MechanicalScanningSonarMessageID::mss_mode);
+}
+
+void Protocol::requestMSSGain()
+{
+    request(Message::MechanicalScanningSonarMessageID::mss_gain);
 }
--- src/protocol/protocol.h
+++ src/protocol/protocol.h
@@ -15,9 +15,64 @@ public:
     Message _message;
     Packer* _packer;
 
+    void request(QVariant messageID);
     void handleData(const QByteArray& data);
+
+    // General requests
     void requestVersion();
+    void requestDeviceID();
+    void requestNewData();
+
+    // Echosounder requests
+    void requestEchosounderDistanceSimple();
+    void requestEchosounderDistance();
+    void requestEchosounderProfile();
+    void requestEchosounderRange();
+    void requestEchosounderMode();
+    void requestEchosounderRate();
+    void requestEchosounderGain();
+    void requestEchosounderPulse();
+
+    // Mechanical Scanning Sonar requests
+    void requestMSSAngleProfilea();
+    void requestMSSRange();
+    void requestMSSMode();
+    void requestMSSGain();
 
 signals:
     void sendData(const QByteArray& data);
+
+    // General info
+    void deviceType(const QString& devType);
+    void deviceModel(const QString& devModel);
+    void firmwareVersion(const QString& fwVersion);
+    void deviceID(int ID);
+    void newData(bool dataAvailable);
+
+    // Echosounder info
+    void EchosounderDistance(int mm);
+    void EchosounderConfidence(float perc);
+    void EchosounderPulseUs(int pulse);
+    void EchosounderPingNumber(int pingNumber);
+    void EchosounderStart(int mm);
+    void EchosounderLength(int mm);
+    void EchosounderGain(int gain);
+    void EchosounderNumberOfPoints(int numberOfPoints);
+    void EchosounderPoints(QList<int> points);
+    void EchosounderMode(int mode);
+    void EchosounderRate(int rate);
+
+    // Mechanical Scanning Sonar info
+    void MSSangle(float angle);
+    void MSSPulseUs(int pulse);
+    void MSSRange(int mm);
+    void MSSGain(int gain);
+    void MSSNumberOfPoints(int numberOfPoints);
+    void MSSPoints(QList<int> points);
+    void MSSMode(int mode);
+    void MSStrainAngle(float angle);
+    void MSSsectorWidth(float angle);
+    void MSSstepSize(int stepSize);
+    void MSSLength(int mm);
+    void MSSsampleSize(int size);
 };
