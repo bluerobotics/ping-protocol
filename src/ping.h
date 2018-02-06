2c13e248fda456f1e7d0e09ee4acc185602d974b
Patrick José Pereira <patrickelectric@gmail.com>
2018-02-06 11:54:36 -0200

    FileLink: Add first version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/ping.h
+++ src/ping.h
@@ -12,17 +12,30 @@ public:
     Protocol* protocol() { return _protocol; };
 
     Q_PROPERTY(AbstractLink* link READ link NOTIFY linkUpdate)
-    AbstractLink* link() { return _link->self(); };
+    AbstractLink* link() { return _linkIn->self(); };
+
+    Q_PROPERTY(AbstractLink* linkLog READ linkLog NOTIFY linkLogUpdate)
+    AbstractLink* linkLog() { return _linkOut->self(); };
 
     Q_INVOKABLE void connectLink(const QString& connString);
+    Q_INVOKABLE void connectLinkLog(const QString& connString);
+
+    Q_INVOKABLE void linkLogPause();
+    Q_INVOKABLE void linkLogStart();
+    Q_INVOKABLE void linkLogStop();
 
 private:
-    Link* _link;
+    Link* _linkIn;
+    Link* _linkOut;
     Protocol* _protocol;
 
 signals:
+    // In
     void connectionClose();
     void connectionOpen();
     void protocolUpdate();
     void linkUpdate();
+
+    // Out
+    void linkLogUpdate();
 };
