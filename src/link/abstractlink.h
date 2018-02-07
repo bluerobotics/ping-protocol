59e3e071503de2a54a31447b754d9ec8d6a3972e
Patrick José Pereira <patrickelectric@gmail.com>
2018-02-07 19:05:16 -0200

    AbstractLink: Add new function for log and replay
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/link/abstractlink.h
+++ src/link/abstractlink.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QObject>
+#include <QTime>
 
 class AbstractLink : public QObject
 {
@@ -41,6 +42,26 @@ public:
     Q_INVOKABLE virtual bool finishConnection() { return true;};
     Q_INVOKABLE virtual bool isOpen() { return true;};
 
+    // Control offline source (E.g: Files)
+    Q_INVOKABLE virtual bool isOnline() { return true; };
+    Q_INVOKABLE virtual void start() {};
+    Q_INVOKABLE virtual void pause() {};
+    Q_PROPERTY(qint64 byteSize READ byteSize NOTIFY byteSizeChanged)
+    Q_INVOKABLE virtual qint64 byteSize() { return 0; };
+    Q_PROPERTY(int packageSize READ packageSize NOTIFY packageSizeChanged)
+    Q_INVOKABLE virtual int packageSize() { return 0; };
+    Q_PROPERTY(int packageIndex READ packageIndex WRITE setPackageIndex NOTIFY packageIndexChanged)
+    Q_INVOKABLE virtual int packageIndex() { return 0; };
+    Q_INVOKABLE virtual void setPackageIndex(int index) { Q_UNUSED(index) };
+    Q_PROPERTY(QTime totalTime READ totalTime NOTIFY totalTimeChanged)
+    Q_INVOKABLE virtual QTime totalTime() { return QTime(); };
+    Q_PROPERTY(QTime elapsedTime READ elapsedTime NOTIFY elapsedTimeChanged)
+    Q_INVOKABLE virtual QTime elapsedTime() { return QTime(); };
+    Q_PROPERTY(QString totalTimeString READ totalTimeString NOTIFY totalTimeChanged)
+    Q_INVOKABLE virtual QString totalTimeString() { return totalTime().toString(_timeFormat); };
+    Q_PROPERTY(QString elapsedTimeString READ elapsedTimeString NOTIFY elapsedTimeChanged)
+    Q_INVOKABLE virtual QString elapsedTimeString() { return elapsedTime().toString(_timeFormat); };
+
     virtual QString errorString() { return QString();};
 
 signals:
@@ -50,6 +71,15 @@ signals:
     void linkChanged(LinkType link);
     void newData(const QByteArray& data);
     void sendData(const QByteArray& data);
+
+    void byteSizeChanged();
+    void packageSizeChanged();
+    void packageIndexChanged();
+    void totalTimeChanged();
+    void elapsedTimeChanged();
+
+protected:
+    static const QString _timeFormat;
 
 private:
     bool _autoConnect;
