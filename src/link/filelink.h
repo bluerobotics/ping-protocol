ea2510c8365c83399a02d95baf71b66f6f7139db
Jacob Walser <jwalser90@gmail.com>
2018-03-14 13:17:47 -0400

    FileLink: Fix multiple inheritance of QObject
    Has a file instead of is a file

--- src/link/filelink.h
+++ src/link/filelink.h
@@ -7,27 +7,28 @@
 #include "abstractlink.h"
 #include "logthread.h"
 
-class FileLink : public AbstractLink, QFile
+class FileLink : public AbstractLink
 {
 public:
     FileLink();
     ~FileLink();
 
-    bool isOpen() final { return isWritable() && isReadable(); };
+    bool isOpen() final { return _file.isWritable() && _file.isReadable(); };
     bool setConfiguration(const QString& arg) final;
     bool startConnection() final;
     bool finishConnection() final;
-    QString errorString() final { return QFile::errorString(); };
+    QString errorString() final { return _file.errorString(); };
 
     bool isOnline() final { return false; };
     void start() final { if(_logThread) _logThread->startJob(); };
     void pause() final { if(_logThread) _logThread->pauseJob(); };
-    qint64 byteSize() final { return bytesAvailable(); };
+    qint64 byteSize() final { return _file.bytesAvailable(); };
     int packageSize() final { return _logThread ? _logThread->packageSize() : 0; };
     int packageIndex() final { return _logThread ? _logThread->packageIndex() : 0; };
     void setPackageIndex(int index) { if(_logThread) _logThread->setPackageIndex(index); }
     QTime totalTime() final { return _logThread ? _logThread->totalTime() : QTime(); };
     QTime elapsedTime() final { return _logThread ? _logThread->elapsedTime() : QTime(); };
+
 private:
     struct Pack {
         QString time;
@@ -36,7 +37,11 @@ private:
 
     QIODevice::OpenModeFlag _openModeFlag;
     QTime _time;
+
+    QFile _file;
     QDataStream _inout;
 
     LogThread* _logThread;
+
+    void _writeData(const QByteArray& data);
 };
