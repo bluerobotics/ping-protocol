7cd8c23cba080cfa34acd5cc1f52c63709e3d740
Patrick José Pereira <patrickelectric@gmail.com>
2018-04-27 09:28:43 -0300

    parser: Create parser_json.cpp with Q_LOGGING_CATEGORY
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/protocol/parsers/parser_json.h
+++ src/protocol/parsers/parser_json.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#include <QDebug>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QJsonValue>
 #include <QObject>
 
 #include "parser.h"
@@ -22,48 +18,9 @@ public:
 
     state_t state;
 
-    uint8_t parseByte(const char& byte) override
-    {
-        static QByteArray buf;
-        switch(state) {
-        case WaitStart:
-            if (byte == '{') {
-                buf.append(byte);
-                state = WaitEnd;
-            } else {
-                errors++;
-            }
-            break;
-        case WaitEnd:
-            buf.append(byte);
-            if (byte == '}') {
-                QJsonDocument doc = QJsonDocument::fromJson(buf);
-                if (doc.isNull()) {
-                    // error
-                    qDebug() << "Error parsing JSON document from buffer";
-                    errors++;
-                } else {
-                    if (!doc.isObject()) {
-                        qDebug() << "Error JSON document is not object";
-                        errors++;
-                    } else {
-                        parsed++;
-                        emit newJsonObject(doc.object());
-                    }
-                }
-                state = WaitStart;
-                buf.clear();
-            }
-            break;
-        }
-        return 0;
-    }
+    uint8_t parseByte(const char& byte) override;
 
-    void parseBuffer(const QByteArray& buf) override {
-        for (int i = 0; i < buf.size(); i++) {
-            parseByte(buf.at(i));
-        }
-    }
+    void parseBuffer(const QByteArray& buf) override;
 
 signals:
     void newJsonObject(const QJsonObject& obj);
