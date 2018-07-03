a3668beb7f6bb984fd75c2178130fe6227efcf8f
Jacob Walser <jwalser90@gmail.com>
2018-07-03 13:14:43 -0400

    protocol: add documentation generation and template

---
+++ src/protocol/generate-doc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import collections
+import json
+import re
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+JINJA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/')
+
+from jinja2 import Environment, FileSystemLoader
+
+jsondata = json.load(open(os.path.join(JINJA_PATH, 'ping_protocol.json'), 'r'), object_pairs_hook=collections.OrderedDict)
+
+j2_env = Environment(loader=FileSystemLoader(JINJA_PATH), trim_blocks=True)
+
+f = open('ping-doc.md', "w")
+f.write(j2_env.get_template("ping_doc.in").render(jsondata))
+f.close()
+
