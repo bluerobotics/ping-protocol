b9f4485a7a2841893f616e4b75d788afed1586b2
Willian Galvani <williangalvani@gmail.com>
2018-10-31 17:17:28 -0300

    generate-python.py: PEP8/cleaning

--- src/protocol/generate-python.py
+++ src/protocol/generate-python.py
@@ -4,15 +4,14 @@ import os
 import collections
 import json
 import re
+from jinja2 import Environment, FileSystemLoader
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 JINJA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/')
 
-from jinja2 import Environment, FileSystemLoader
-
-jsondata = json.load(open(os.path.join(JINJA_PATH, 'ping_protocol.json'), 'r'), object_pairs_hook=collections.OrderedDict)
-
+json_data = json.load(open(os.path.join(JINJA_PATH, 'ping_protocol.json'), 'r'), object_pairs_hook=collections.OrderedDict)
 j2_env = Environment(loader=FileSystemLoader(JINJA_PATH), trim_blocks=True)
+
 
 def calc_payload(payloads):
     total_size = 0
@@ -48,25 +47,25 @@ def get_c_size(t):
 def is_vector(payload):
     return ('vector' in payload)
 
-structToken = { "u8":"B",
-                "u16":"H",
-                "u32":"I",
-                "i8":"b",
-                "i16":"h",
-                "i32":"i",
-                "char":"s" }
+struct_token = {"u8": "B",
+                "u16": "H",
+                "u32": "I",
+                "i8": "b",
+                "i16": "h",
+                "i32": "i",
+                "char": "s"}
 
 # Create a new dictionary with all message keys for convenience in the template
-messageDict = jsondata['messages']['ping1D']
+messageDict = json_data['messages']['ping1D']
 
 allMessages = dict(messageDict['get'])
 allMessages.update(messageDict['set'])
 allMessages.update(messageDict['control'])
 allMessages.update(messageDict['general'])
 
-jsondata['all_msgs'] = allMessages
+json_data['all_msgs'] = allMessages
 
-j2_env.globals['structToken'] = structToken
+j2_env.globals['structToken'] = struct_token
 j2_env.globals.update(calc_payload = calc_payload)
 j2_env.globals.update(get_c_size = get_c_size)
 j2_env.globals.update(is_vector = is_vector)
@@ -77,9 +76,9 @@ if not os.path.exists(output_path):
     os.makedirs(output_path)
 
 f = open(os.path.join(output_path, "pingmessage.py"), "w")
-f.write(j2_env.get_template("pingmessage.py.in").render(jsondata))
+f.write(j2_env.get_template("pingmessage.py.in").render(json_data))
 f.close()
 
 f = open(os.path.join(output_path, "ping1d.py"), "w")
-f.write(j2_env.get_template("ping1d.py.in").render(jsondata))
+f.write(j2_env.get_template("ping1d.py.in").render(json_data))
 f.close()
