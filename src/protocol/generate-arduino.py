62fe14f63c2d5d5052d640e7c621a6cbc276db31
Patrick José Pereira <patrickelectric@gmail.com>
2019-03-26 17:08:17 -0300

    generate-arduino: Move script to be inside main
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/protocol/generate-arduino.py
+++ src/protocol/generate-arduino.py
@@ -5,46 +5,47 @@ import collections
 import json
 import re
 
-from generator import Generator
-
-PATH = os.path.dirname(os.path.abspath(__file__))
-JINJA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/')
-
 from jinja2 import Environment, FileSystemLoader
 
-jsondata = json.load(open(os.path.join(JINJA_PATH, 'ping_protocol.json'), 'r'), object_pairs_hook=collections.OrderedDict)
+from generator import Generator
 
-j2_env = Environment(loader=FileSystemLoader(JINJA_PATH), trim_blocks=True)
+if __name__ == "__main__":
+    PATH = os.path.dirname(os.path.abspath(__file__))
+    JINJA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/')
 
-# Create a new dictionary with all message keys for convenience in the template
-messageDict = jsondata['messages']['ping1D']
+    jsondata = json.load(open(os.path.join(JINJA_PATH, 'ping_protocol.json'), 'r'), object_pairs_hook=collections.OrderedDict)
 
-allMessages = dict(messageDict['get'])
-allMessages.update(messageDict['set'])
-allMessages.update(messageDict['control'])
-allMessages.update(messageDict['general'])
+    j2_env = Environment(loader=FileSystemLoader(JINJA_PATH), trim_blocks=True)
 
-# compile a list of all payload fields in 'get' category messages
-# these field names will be members of the ping1d device interface
-proplist = {}
-for msg in messageDict['get']:
-    for field in messageDict['get'][msg]['payload']:
-        proplist[field['name']] = field
+    # Create a new dictionary with all message keys for convenience in the template
+    messageDict = jsondata['messages']['ping1D']
 
-jsondata['all_msgs'] = allMessages
-jsondata['proplist'] = proplist
+    allMessages = dict(messageDict['get'])
+    allMessages.update(messageDict['set'])
+    allMessages.update(messageDict['control'])
+    allMessages.update(messageDict['general'])
 
-j2_env.globals.update(generator=Generator())
+    # compile a list of all payload fields in 'get' category messages
+    # these field names will be members of the ping1d device interface
+    proplist = {}
+    for msg in messageDict['get']:
+        for field in messageDict['get'][msg]['payload']:
+            proplist[field['name']] = field
 
-# Create output file
-output_path = os.path.join(PATH, "arduino/")
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
+    jsondata['all_msgs'] = allMessages
+    jsondata['proplist'] = proplist
 
-f = open(os.path.join(output_path, "ping1d.h"), "w")
-f.write(j2_env.get_template("ping1d.h.in").render(jsondata))
-f.close()
+    j2_env.globals.update(generator=Generator())
 
-f = open(os.path.join(output_path, "ping1d.cpp"), "w")
-f.write(j2_env.get_template("ping1d.cpp.in").render(jsondata))
-f.close()
+    # Create output file
+    output_path = os.path.join(PATH, "arduino/")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    f = open(os.path.join(output_path, "ping1d.h"), "w")
+    f.write(j2_env.get_template("ping1d.h.in").render(jsondata))
+    f.close()
+
+    f = open(os.path.join(output_path, "ping1d.cpp"), "w")
+    f.write(j2_env.get_template("ping1d.cpp.in").render(jsondata))
+    f.close()
