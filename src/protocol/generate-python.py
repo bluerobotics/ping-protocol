804250d847bacf4264ddd2b493cfd3d71cebe0da
Patrick José Pereira <patrickelectric@gmail.com>
2019-03-26 17:12:43 -0300

    generate-python: Use generator class
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/protocol/generate-python.py
+++ src/protocol/generate-python.py
@@ -6,46 +6,13 @@ import json
 import re
 from jinja2 import Environment, FileSystemLoader
 
+from generator import Generator
+
 PATH = os.path.dirname(os.path.abspath(__file__))
 JINJA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/')
 
 json_data = json.load(open(os.path.join(JINJA_PATH, 'ping_protocol.json'), 'r'), object_pairs_hook=collections.OrderedDict)
 j2_env = Environment(loader=FileSystemLoader(JINJA_PATH), trim_blocks=True)
-
-
-def calc_payload(payloads):
-    total_size = 0
-    for payload in payloads:
-        if is_vector(payload["type"]):
-
-            if payload["vector"]["size"] == "dynamic":
-                if "sizetype" in payload["vector"]:
-                    total_size = total_size + get_c_size(payload["vector"]["sizetype"])
-            else:
-                total_size = total_size + int(payload["vector"]["size"]) * get_c_size(payload["vector"]["datatype"])
-
-        else:
-            total_size = total_size + get_c_size(payload["type"])
-
-    return total_size
-
-def get_c_size(t):
-    # this regex will get the X in u/intX_t (uint8_t, int16_t)
-    match = re.search('[0-9]{1,2}', t)
-    if match:
-        return int(int(match.group(0)) / 8)
-
-    if t.find('bool') != -1:
-        return 1
-    if t.find('int') != -1:
-        return 4
-    if t.find('float') != -1:
-        return 4
-    if t.find('double') != -1:
-        return 8
-
-def is_vector(payload):
-    return ('vector' in payload)
 
 struct_token = {"u8": "B",
                 "u16": "H",
@@ -71,9 +38,7 @@ json_data['all_msgs'] = allMessages
 json_data['all_fields'] = list(set([msg['name'] for key, message in allMessages.items() for msg in message['payload']]))
 
 j2_env.globals['structToken'] = struct_token
-j2_env.globals.update(calc_payload = calc_payload)
-j2_env.globals.update(get_c_size = get_c_size)
-j2_env.globals.update(is_vector = is_vector)
+j2_env.globals.update(generator=Generator())
 
 # Create output file
 output_path = os.path.join(PATH, "python/")
