e2906eb323f7421ee3f596a8f627ddcc6a93d56b
Patrick José Pereira <patrickelectric@gmail.com>
2019-03-28 11:16:16 -0300

    generate-arduino: Use generator default paths
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/protocol/generate-arduino.py
+++ src/protocol/generate-arduino.py
@@ -10,12 +10,8 @@ from jinja2 import Environment, FileSystemLoader
 from generator import Generator
 
 if __name__ == "__main__":
-    PATH = os.path.dirname(os.path.abspath(__file__))
-    JINJA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/')
-
-    jsondata = json.load(open(os.path.join(JINJA_PATH, 'ping_protocol.json'), 'r'), object_pairs_hook=collections.OrderedDict)
-
-    j2_env = Environment(loader=FileSystemLoader(JINJA_PATH), trim_blocks=True)
+    jsondata = json.load(open(os.path.join(Generator.RECIPE_PATH, 'ping_protocol.json'), 'r'), object_pairs_hook=collections.OrderedDict)
+    j2_env = Environment(loader=FileSystemLoader(Generator.JINJA_PATH), trim_blocks=True)
 
     # Create a new dictionary with all message keys for convenience in the template
     messageDict = jsondata['messages']['ping1D']
@@ -38,7 +34,7 @@ if __name__ == "__main__":
     j2_env.globals.update(generator=Generator())
 
     # Create output file
-    output_path = os.path.join(PATH, "arduino/")
+    output_path = os.path.join(Generator.PATH, "arduino/")
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
