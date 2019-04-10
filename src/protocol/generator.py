0d69b886f707617c83cbde66cbf3bf7ed9d24d7e
Patrick José Pereira <patrickelectric@gmail.com>
2019-04-10 17:58:03 -0300

    generator: Use new structure
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/protocol/generator.py
+++ src/protocol/generator.py
@@ -113,25 +113,25 @@ if __name__ == "__main__":
     for json_file in jsons:
         # Get json data
         protocol_data = json.load(open(os.path.join(Generator.RECIPE_PATH, json_file), 'r'))
-        print('Processing file: %s' % protocol_data['class_info']['file'])
+        print('Processing file: %s' % protocol_data['class_info']['name'])
+
         # Create prefix name
         file_prefix_name = protocol_data['class_info']['name'].lower()
         class_names.append(file_prefix_name)
 
         # Create output file
-        output_path = os.path.join(Generator.PATH, '{0}/'.format(file_prefix_name))
+        output_path = os.path.join(Generator.PATH, '{0}/'.format('pingmessage'))
         if not os.path.exists(output_path):
             os.makedirs(output_path)
 
         # Create a new dictionary with all message keys for convenience in the template
-        messageDict = protocol_data['messages']['ping1D']
+        messageDict = protocol_data['messages']
+        allMessages = {}
 
-        allMessages = dict(messageDict['get'])
-        allMessages.update(messageDict['set'])
-        allMessages.update(messageDict['control'])
-        allMessages.update(messageDict['general'])
+        for message_type in messageDict.keys():
+            allMessages.update(messageDict[message_type])
 
-        protocol_data['messages']['ping1D']['all_msgs'] = allMessages
+        protocol_data['messages']['all_msgs'] = allMessages
 
         # Create our lovely jinja env
         j2_env = Environment(loader=FileSystemLoader(Generator.JINJA_PATH), trim_blocks=True)
@@ -141,16 +141,4 @@ if __name__ == "__main__":
         # Create main class
         f = open(os.path.join(output_path, "{0}.h".format(file_prefix_name)), "w")
         f.write(j2_env.get_template(protocol_data['class_info']['file']).render(protocol_data))
-        f.close()
-
-        # Create subclass
-        for message_type in protocol_data["messages"]:
-            j2_env.globals.update(_actual_message_type=message_type)
-            f = open(os.path.join(output_path, "{0}_{1}.h".format(file_prefix_name, message_type)), "w")
-            f.write(j2_env.get_template(protocol_data['subclass_info']['file']).render(protocol_data))
-            f.close()
-
-        # Create all header with all subclasses
-        f = open(os.path.join(output_path, "{0}_all.h".format(file_prefix_name)), "w")
-        f.write(j2_env.get_template('_all.h.in').render(protocol_data))
         f.close()
