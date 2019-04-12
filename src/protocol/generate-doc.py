c447bd97e65dae5ddb8b6d39647696d346899aa5
Patrick José Pereira <patrickelectric@gmail.com>
2019-04-12 12:27:10 -0300

    generate-doc: Use new Generator class
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/protocol/generate-doc.py
+++ src/protocol/generate-doc.py
@@ -4,14 +4,13 @@ import os
 import collections
 import json
 
-PATH = os.path.dirname(os.path.abspath(__file__))
-JINJA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/')
-
 from jinja2 import Environment, FileSystemLoader
 
-jsondata = json.load(open(os.path.join(JINJA_PATH, 'ping_protocol.json'), 'r'), object_pairs_hook=collections.OrderedDict)
+from generator import Generator
 
-j2_env = Environment(loader=FileSystemLoader(JINJA_PATH), trim_blocks=True)
+jsondata = json.load(open(os.path.join(Generator.RECIPE_PATH, 'ping_protocol.json'), 'r'), object_pairs_hook=collections.OrderedDict)
+
+j2_env = Environment(loader=FileSystemLoader(Generator.JINJA_PATH), trim_blocks=True)
 
 f = open('README.md', "w")
 f.write(j2_env.get_template("ping_doc.in").render(jsondata))
