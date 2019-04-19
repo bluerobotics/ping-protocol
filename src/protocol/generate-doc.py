7c8f9ee67b0afcb63386396f62b7f387600b7466
Patrick José Pereira <patrickelectric@gmail.com>
2019-04-19 11:11:16 -0300

    generate-doc: Move from recipes to definitions
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/protocol/generate-doc.py
+++ src/protocol/generate-doc.py
@@ -9,12 +9,11 @@ from jinja2 import Environment, FileSystemLoader
 from generator import Generator
 
 if __name__ == "__main__":
-    recipes = [file for file in os.listdir(Generator.RECIPE_PATH) if file.endswith('.json')]
-    recipes.sort()
+    definitions = [file for file in os.listdir(Generator.DEFINITION_PATH) if file.endswith('.json')]
+    definitions.sort()
     jsondata = collections.OrderedDict({ "sensors": {} })
-
-    for recipe in recipes:
-        data = json.load(open(os.path.join(Generator.RECIPE_PATH, recipe), 'r'), object_pairs_hook=collections.OrderedDict)
+    for definition in definitions:
+        data = json.load(open(os.path.join(Generator.DEFINITION_PATH, definition), 'r'), object_pairs_hook=collections.OrderedDict)
         jsondata['sensors'][data['sensor_info']['name']] = data
 
     j2_env = Environment(loader=FileSystemLoader(Generator.JINJA_PATH), trim_blocks=True)
