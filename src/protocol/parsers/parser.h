859af9f4b0623866243a7d3d9de69944bb45730a
Patrick José Pereira <patrickelectric@gmail.com>
2019-06-17 17:53:11 -0300

    parser: Make functions pure virtual
    Parser is not useful without such functions
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/protocol/parsers/parser.h
+++ src/protocol/parsers/parser.h
@@ -15,6 +15,6 @@ public:
     uint32_t parsed; // number of messages/packets successfully parsed
     uint32_t errors; // number of parse errors
 
-    virtual void parseBuffer(const QByteArray& data) { Q_UNUSED(data)} // asynchronous use, Child classes should signal when something happens ie. 'emit newMessage(Message m)'
-    virtual uint8_t parseByte(const char byte) { Q_UNUSED(byte) return 0; } // synchronous use, Child should return flags indicating incremental parse result/status
+    virtual void parseBuffer(const QByteArray& data) = 0; // asynchronous use, Child classes should signal when something happens ie. 'emit newMessage(Message m)'
+    virtual uint8_t parseByte(const char byte) = 0; // synchronous use, Child should return flags indicating incremental parse result/status
 };
