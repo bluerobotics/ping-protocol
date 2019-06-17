ed10cb22fa1e296b96cacf29f57c354343d0bf86
Patrick José Pereira <patrickelectric@gmail.com>
2019-06-17 17:53:55 -0300

    parser: Add pingMessage
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/protocol/parsers/parser.h
+++ src/protocol/parsers/parser.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <QObject>
+#include "pingmessage/ping_message.h"
 
 // This class digests data and notifies owner when something interesting happens
 class Parser : public QObject
@@ -17,4 +18,11 @@ public:
 
     virtual void parseBuffer(const QByteArray& data) = 0; // asynchronous use, Child classes should signal when something happens ie. 'emit newMessage(Message m)'
     virtual uint8_t parseByte(const char byte) = 0; // synchronous use, Child should return flags indicating incremental parse result/status
+
+    /**
+     * @brief Return the last valid ping message
+     *
+     * @return ping_message
+     */
+    virtual ping_message pingMessage() const = 0;
 };
